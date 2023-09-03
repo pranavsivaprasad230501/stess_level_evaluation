67437ceda702eca881b80904095a9deb44fdbe96
pranav230501 <1at19is071@visioneer.atria.edu>
2023-09-03 11:30:17 +0530

    first commit

---
+++ Stress FINAL NEW/educationalProject/educationalProject/_Graph.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Web.UI.DataVisualization.Charting;
+
+namespace educationalProject
+{
+    public partial class _Graph : System.Web.UI.Page
+    {
+        Dictionary<string, double> testData = new Dictionary<string, double>();
+
+        protected override void OnLoad(EventArgs e)
+        {
+            try
+            {               
+                base.OnLoad(e);
+
+                if (!IsPostBack)
+                {
+                    // bind chart type names to ddl
+                    ddlChartType.DataSource = Enum.GetNames(typeof(SeriesChartType));
+                    ddlChartType.DataBind();
+
+                    //cbUse3D.Checked = true;
+                }
+
+                DataBind();
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        protected override void OnDataBinding(EventArgs e)
+        {
+            base.OnDataBinding(e);
+            testData.Clear();
+
+
+            testData.Add("NaiveBayes", 90.3);           
+
+            ccTestChart.Series["Testing"].Points.DataBind(testData, "Key", "Value", string.Empty);
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+
+            // update chart rendering           
+            ccTestChart.Series["Testing"].ChartTypeName = "Column";
+
+            ccTestChart.ChartAreas[0].Area3DStyle.Enable3D = cbUse3D.Checked;
+            ccTestChart.ChartAreas[0].Area3DStyle.Inclination = Convert.ToInt32(rblInclinationAngle.SelectedValue);
+
+            ccTestChart.Visible = true;
+        }
+
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+            ccTestChart.Visible = true;
+
+            OnDataBinding(e);
+            OnPreRender(e);
+        }
+               
+    }
+}
